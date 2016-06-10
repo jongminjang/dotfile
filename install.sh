57c3db48cbf0fbefd6e853460ad363a9611fe6d7
Jongmin Jang <mirth81@gmail.com>
2016-06-10 17:29:11 +0900

    added install.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/zshenv ~/.zshenv
+ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/init.el ~/.emacs.d/personal
+ln -s ~/dotfiles/mypreload.el ~/.emacs.d/personal/preload
