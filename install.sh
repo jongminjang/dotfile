76871a2cd392ad76a631b77a995bc0022ddc685f
Jongmin Jang <mirth81@gmail.com>
2017-10-21 23:23:59 +0900

    j

--- install.sh
+++ install.sh
@@ -2,6 +2,3 @@
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/zshenv ~/.zshenv
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/init.el ~/.emacs.d/personal
-ln -s ~/dotfiles/custom.el ~/.emacs.d/personal
-ln -s ~/dotfiles/mypreload.el ~/.emacs.d/personal/preload
